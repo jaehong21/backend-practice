9a8accfe9e04cf35a4350e4308ef6d2cbff59f7c
jjay2002 <jaehong21@gm.gist.ac.kr>
2021-08-31 14:57:07 +0900

    part 3 complete

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -1,10 +1,20 @@
+from django import template
 from django.http import HttpResponse
+from django.shortcuts import render, get_object_or_404
+
+from polls.models import Question
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the polls index.")
+    lastest_question_list = Question.objects.order_by('pub_date')[:5]
+    context = {'lastest_question_list': lastest_question_list,}
+    # output = ', '.join([q.question_text for q in lastest_question_list])
+    # return HttpResponse("Hello, world. You're at the polls index.")
+    return render(request, 'polls/index.html', context)
 
 def detail(request, question_id): 
-    return HttpResponse("You're looking at question %s." % question_id)
+    # return HttpResponse("You're looking at question %s." % question_id)
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, 'polls/detail.html', {'question': question},)
 
 def results(request, question_id):
     response = "You're looking at the results of question %s."
